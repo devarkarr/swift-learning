e9fa3239d0050fc57726921ff8bd8c91b029e6cf
arkarlin2003 <arkarlin486@gmail.com>
2026-04-08 11:15:18 +0630

    Initial Commit

---
+++ project-1/project_1App.swift
@@ -0,0 +1,17 @@
+//
+//  project_1App.swift
+//  project-1
+//
+//  Created by Ar Kar Lin on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct project_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
