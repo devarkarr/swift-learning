a32f7c838c934fc981353b921b1f89e689d73f45
arkarlin2003 <arkarlin486@gmail.com>
2026-04-08 15:38:37 +0630

    Buttons and state

---
+++ project-1/DiceView/DiceView.swift
@@ -0,0 +1,18 @@
+//
+//  DiceView.swift
+//  project-1
+//
+//  Created by Ar Kar Lin on 4/8/26.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceView()
+}
---
+++ project-1/DiceView/DiceViewPage.swift
@@ -0,0 +1,18 @@
+//
+//  DiceViewPage.swift
+//  project-1
+//
+//  Created by Ar Kar Lin on 4/8/26.
+//
+
+import SwiftUI
+
+struct DiceViewPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiceViewPage()
+}
